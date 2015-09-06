7326b6416b5ae50cd520374209a2604342a49749
DiveDave <yxu60@binghamton.edu>
2015-09-06 13:37:57 -0700

    Target file path issue fixed.
    When calling "graspurl", user can either assign a target folder name or not.
    If target folder name is assigned, then target file will be stored in the assigned target folder (under current working path),
    if not, a new folder named after current date (YYYY_mm_dd) will be created under current working path (if it doesn't exist) and the target file will be stored in it.

--- src/grasper.py
+++ src/grasper.py
@@ -1,8 +1,24 @@
+import os
+import datetime
 import urllib
 import urllib2
 import cookielib
 
-def graspurl(targetUrl):
+
+def GetDateStr():
+    from datetime import date
+    dateStr = date.today().strftime("%Y_%m_%d")
+    return dateStr
+
+def MkNewDir(newDir=None):
+	if newDir is None:
+		newDirCreated = "./" + GetDateStr()
+	else:
+		newDirCreated = "./" + newDir
+	os.mkdir(newDirCreated)
+	return newDirCreated
+
+def graspurl(targetUrl, targetDir=None):
 	ck = cookielib.LWPCookieJar()
 	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(ck))
 	urllib2.install_opener(opener)
@@ -10,11 +26,16 @@ def graspurl(targetUrl):
 	req = urllib2.Request(targetUrl)
 	operate = opener.open(req)
 	msg = operate.read()
-	
-	fullpath = "D://"
+
+	if targetDir and os.path.isdir("./"+targetDir):
+		print(targetDir," Exists already ")
+		fullpath = os.getcwd() + "/" + targetDir
+	else:
+		fullpath = os.getcwd() + MkNewDir(targetDir)
+
 	filename = targetUrl.replace('/','_')
 	filename = filename.replace(':','.')
-	filename = fullpath + filename + '.html'
+	filename = fullpath + "/" + filename + '.html'
 	
 	file_obj = open(filename, 'w')
 	file_obj.write(msg)
